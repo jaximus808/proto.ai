578bc3ac24126809738b9100c89b1a105e9a90dd
Andria Luo <a.s.luo@wustl.edu>
2024-10-20 11:59:52 -0500

    about

--- proto_web/app/about/page.tsx
+++ proto_web/app/about/page.tsx
@@ -13,8 +13,7 @@ const About: React.FC = () => {
     <title>About</title>
     <br></br>
       <h1>About Us</h1>
-      <br></br><br></br>
-      <div className='blurb'><img src="ai.jxl" alt="Picture of robot"></img></div>
+      <br></br>
       <div className='blurb'>
       <p>
         Proto.ai was designed to leverage cutting-edge AI to empower developers by simplifying the creation of server-client architectures. Our platform eliminates the guesswork that often comes with interacting with language models (LLMs), offering precise, actionable insights to design, write, and optimize code. Whether building APIs, managing data flows, or solving communication challenges, we help developers focus on innovation while ensuring seamless server-client relations.
@@ -22,6 +21,7 @@ const About: React.FC = () => {
       </div>
       <div className='wrapper'>
             <h2>Mission Statement</h2>
+            <br></br>
             <p>Our mission is to empower developers and businesses by revolutionizing the way they design and build server-client applications. Through the use of advanced AI technology, we eliminate the guesswork associated with interacting with language models. Our platform delivers precise, actionable solutions, helping users write and refine code effortlessly, so they can focus on innovation rather than troubleshooting. We aim to simplify the complexities of backend and frontend communications, fostering seamless integrations, improved productivity, and scalable solutions for the future of software development.</p>
         </div>
         <br></br>
@@ -29,10 +29,11 @@ const About: React.FC = () => {
         <br></br>
         <div className="wrapper">
         <h2>The People</h2>
+        <br></br>
           <h3>Jaxon Poentis</h3>
-          <p>uh</p>
+          <p>Sophomore in Computer Science and Math at Washington University in St. Louis</p>
           <h3>Andria Luo</h3>
-          <p>uh</p>
+          <p>Sophomore in Computer Science at Washington University in St. Louis</p>
         </div>
         <br></br>
         
